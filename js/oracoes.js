ac41520c2df61680ad567af9a9185728e62a4e61
Arthur Castro <arthur.castro07@gmail.com>
2020-05-18 19:53:22 -0300

    Correcoes nos textos das oracoes

--- js/oracoes.js
+++ js/oracoes.js
@@ -38,7 +38,7 @@ const misterios = [
 
 const tresAveMarias = [
    "",     // Perda de memória para facilitar a contagem do array
-   "A primeira Ave-Maria em honra a Deus Pai que nos criou",
+   "A primeira Ave Maria em honra a Deus Pai que nos criou",
    "A segunda Ave Maria a Deus Filho que nos remiu",
    "A terceira Ave Maria ao Espírito Santo que nos santifica"
 ]
@@ -56,21 +56,21 @@ const tituloOracao = [
 ]
 
 const oracao = [
-   "Em nome do Pai, do Filho e do Espírito Santo.\nAmém.",
+   "Em nome do Pai, do Filho e do Espírito Santo.<br>Amém.",
 
-   "Divino Jesus,\neu vos ofereço este terço (Rosário) que vou rezar, contemplando os mistérios de nossa Redenção. Concedei-me, pela intercessão de Maria, vossa Mãe Santíssima, a quem me dirijo, as graças necessárias para bem rezá-lo para ganhar as indulgências desta santa devoção.",
+   "Divino Jesus, eu vos ofereço este terço (Rosário) que vou rezar, contemplando os mistérios de nossa Redenção. Concedei-me, pela intercessão de Maria, vossa Mãe Santíssima, a quem me dirijo, as graças necessárias para bem rezá-lo para ganhar as indulgências desta santa devoção.",
 
-   "Creio\nem Deus Pai todo-poderoso, Criador do céu e da terra, e em Jesus Cristo, seu único Filho, nosso Senhor, que foi concebido pelo poder do Espírito Santo, nasceu da Virgem Maria, padeceu sob Pôncio Pilatos, foi crucificado, morto e sepultado; desceu à mansão dos mortos; ressuscitou ao terceiro dia; subiu aos céus, está sentado à direita de Deus Pai todo poderoso, donde há de vir julgar os vivos e os mortos. Creio no Espírito Santo, na Santa Igreja Católica, na comunhão dos santos, na remissão dos pecados, na ressurreição da carne e na vida eterna.\nAmém.",
+   "Creio em Deus Pai todo-poderoso, Criador do céu e da terra, e em Jesus Cristo, seu único Filho, nosso Senhor, que foi concebido pelo poder do Espírito Santo, nasceu da Virgem Maria, padeceu sob Pôncio Pilatos, foi crucificado, morto e sepultado; desceu à mansão dos mortos; ressuscitou ao terceiro dia; subiu aos céus, está sentado à direita de Deus Pai todo poderoso, donde há de vir julgar os vivos e os mortos. Creio no Espírito Santo, na Santa Igreja Católica, na comunhão dos santos, na remissão dos pecados, na ressurreição da carne e na vida eterna.<br>Amém.",
 
-   "Pai Nosso\nque estais nos céus, santificado seja o Vosso nome, venha a nós o Vosso reino, seja feita a vossa vontade, assim na terra como no céu. O pão nosso de cada dia nos dai hoje; perdoai-nos as nossa ofensas, assim como nós perdoamos a quem nos tem ofendido e não nos deixeis cair em tentação, mas livrai-nos do mal.\nAmém.",
+   "Pai Nosso que estais nos céus, santificado seja o Vosso nome, venha a nós o Vosso reino, seja feita a vossa vontade, assim na terra como no céu. O pão nosso de cada dia nos dai hoje; perdoai-nos as nossa ofensas, assim como nós perdoamos a quem nos tem ofendido e não nos deixeis cair em tentação, mas livrai-nos do mal.<br>Amém.",
 
-   "Ave Maria\ncheia de graça, o Senhor é convosco, bendita sois vós entre as mulheres e bendito é o fruto do vosso ventre Jesus. Santa Maria Mãe de Deus, rogai por nós pecadores, agora e na hora de nossa morte.\nAmém.",
+   "Ave Maria, cheia de graça, o Senhor é convosco, bendita sois vós entre as mulheres e bendito é o fruto do vosso ventre Jesus. Santa Maria Mãe de Deus, rogai por nós pecadores, agora e na hora de nossa morte.<br>Amém.",
 
-   "Glória ao Pai,\nao Filho e o Espírito Santo. Como era no princípio, agora e sempre.\nAmém.",
+   "Glória ao Pai, ao Filho e o Espírito Santo. Como era no princípio, agora e sempre.<br>Amém.",
 
-   "Oh! Meu Jesus,\nperdoai-nos, livrai-nos do fogo do inferno, levai as almas todas para o céu e socorrei principalmente as que mais precisarem.\nAmém.",
+   "Oh! Meu Jesus, perdoai-nos, livrai-nos do fogo do inferno, levai as almas todas para o céu e socorrei principalmente as que mais precisarem.<br>Amém.",
 
-   "Infinitas graças vos damos, Soberana Rainha, pelos benefícios que todos os dias recebemos de vossa mão liberais. Dignai-vos, agora e para sempre, tomar-nos debaixo do vosso poderoso amparo e para mais vos obrigar vos saudamos com uma Salve Rainha...",
+   "Infinitas graças vos damos, Soberana Rainha, pelos benefícios que todos os dias recebemos de vossas mãos liberais. Dignai-vos, agora e para sempre, tomar-nos debaixo do vosso poderoso amparo e para mais vos obrigar vos saudamos com uma Salve Rainha...",
 
-   "Salve Rainha,\nMãe de Misericórdia, vida, doçura, esperança nossa, salve! A vós bradamos, os degredados filhos de Eva; a vós suspiramos gemendo e chorando neste vale de lágrimas. Eia, pois, advogada nossa esses vossos olhos misericordiosos a nós volvei, e depois deste desterro nos mostrai a Jesus, bendito fruto do vosso ventre, ó Clemente, ó Piedosa, ó Doce, sempre virgem Maria.\n\nV. Rogai por nós, Santa Mãe de Deus,\nR. Para que sejamos dignos das promessas de Cristo.\n\nAmém."
+   "Salve Rainha, Mãe de Misericórdia, vida, doçura, esperança nossa, salve! A vós bradamos, os degredados filhos de Eva; a vós suspiramos gemendo e chorando neste vale de lágrimas. Eia, pois, advogada nossa esses vossos olhos misericordiosos a nós volvei, e depois deste desterro nos mostrai a Jesus, bendito fruto do vosso ventre, ó Clemente, ó Piedosa, ó Doce, sempre virgem Maria.<br><br>V. Rogai por nós, Santa Mãe de Deus,<br>R. Para que sejamos dignos das promessas de Cristo.<br><br>Amém."
 ];
